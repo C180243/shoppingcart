4af8b5bb2b6c1cae925fa625d27ee556f2aa4f40
C180243/shoppingcart <c180243@ccg.ac.jp>
2019-11-26 08:17:18 +0000

    11/26

--- cart.php
+++ cart.php
@@ -48,20 +48,42 @@ if ($mode == "clear") {
 				<h3>選択された商品一覧</h3>
 	<table>
 	    <caption><a href="entry.php?id=<?= $i ?>">検索画面に戻る</a> <a href="cart.php?id=<?= $i ?>">カートの中身を見る</a></caption>
+	    
 		<tr>
 			<th>書籍名</th>
 			<th>価格</th>
 			<th>著者</th>
 			<th>ISBN</th>
 			<th></th>
-		<?php for ($i =0; $i < $items; $i++) { ?>	 
+		<?php  { ?>	 
 		</tr>
 		<tr>
-		<td><?= $cart[$i]->getName() ?></td>
-		<td><?= $cart[$i]->getPrice() ?></td>
-		<td><?= $cart[$i]->getAuthor() ?></td>
-		<td><?= $cart[$i]->getCode() ?></td>
-		</tr>
+						<td>Head First PHP & MySQL</td>
+						<td>4,650</td>
+						<td>Lynn Beighley</td>
+						<td>978-4873114446</td>
+					</tr>
+					<tr>
+						<td>リーダブルコード</td>
+						<td>2,600</td>
+						<td>Dustin Boswell</td>
+						<td>978-4873115658</td>
+						
+					</tr>
+					<tr>
+						<td>Head First デザインパターン</td>
+						<td>5,060</td>
+						<td>Eric Freeman</td>
+						<td>978-4873112497</td>
+						
+					</tr>
+					<tr>
+						<td>PHPによるデザインパターン入門</td>
+						<td>2,400</td>
+						<td>下岡 秀幸</td>
+						<td>978-4798015163</td>
+						
+					</tr>
 		<?php } ?>
 			</table>
 			
